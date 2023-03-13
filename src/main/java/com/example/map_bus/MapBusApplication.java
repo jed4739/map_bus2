0e7713e785843fd444d683299ae1ea67f89ed30e
jed4739 <tjsdlr04@gmail.com>
2023-03-13 17:58:33 +0900

    ddd

---
+++ src/main/java/com/example/map_bus/MapBusApplication.java
@@ -0,0 +1,13 @@
+package com.example.map_bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapBusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/map_bus/MapBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.map_bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
